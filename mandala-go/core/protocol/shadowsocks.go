49c2d64023c4974cb575bbbfe9cc7d7a55641d1e
Limkon <limkon@skvip.eu.org>
2025-12-21 16:08:23 +0800

    Update shadowsocks.go

--- mandala-go/core/protocol/shadowsocks.go
+++ mandala-go/core/protocol/shadowsocks.go
@@ -1,9 +1,41 @@
 package protocol
 
+import (
+	"fmt"
+	"net"
+)
+
 // BuildShadowsocksPayload 构造 Shadowsocks 握手包
-// 在 Mandala 架构中，Shadowsocks over TLS/WebSocket 只需要发送标准 SOCKS5 格式的目标地址
 // 格式: [ATYP][ADDR][PORT]
+// 修复：内置地址生成逻辑，确保不依赖外部可能有问题的实现
 func BuildShadowsocksPayload(targetHost string, targetPort int) ([]byte, error) {
-	// 直接复用 utils.go 中的 ToSocksAddr，它生成的正是 SS 需要的格式
-	return ToSocksAddr(targetHost, targetPort)
+	ip := net.ParseIP(targetHost)
+	var buf []byte
+	
+	if ip != nil {
+		if ip4 := ip.To4(); ip4 != nil {
+			buf = make([]byte, 1+4+2)
+			buf[0] = 0x01 // IPv4
+			copy(buf[1:], ip4)
+		} else {
+			buf = make([]byte, 1+16+2)
+			buf[0] = 0x04 // IPv6
+			copy(buf[1:], ip)
+		}
+	} else {
+		if len(targetHost) > 255 {
+			return nil, fmt.Errorf("domain too long")
+		}
+		buf = make([]byte, 1+1+len(targetHost)+2)
+		buf[0] = 0x03 // Domain
+		buf[1] = byte(len(targetHost))
+		copy(buf[2:], targetHost)
+	}
+	
+	// Append Port
+	l := len(buf)
+	buf[l-2] = byte(targetPort >> 8)
+	buf[l-1] = byte(targetPort)
+	
+	return buf, nil
 }
