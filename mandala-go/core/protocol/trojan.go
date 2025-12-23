05889fab2b55fee75601e3e58d041ad11dd7d924
Limkon <limkon@skvip.eu.org>
2025-12-23 14:15:17 +0800

    Update trojan.go

--- mandala-go/core/protocol/trojan.go
+++ mandala-go/core/protocol/trojan.go
@@ -2,23 +2,28 @@ package protocol
 
 import (
 	"bytes"
+	"log"
 )
 
 // BuildTrojanPayload 构造标准 Trojan 握手包
 // 结构: Hash(pass) + CRLF + CMD(1) + SOCKS5_ADDR + CRLF
 func BuildTrojanPayload(password, targetHost string, targetPort int) ([]byte, error) {
+	log.Printf("[Trojan] 正在构造握手包 -> %s:%d", targetHost, targetPort)
 	var buf bytes.Buffer
 
-	// 1. 密码哈希 (使用 crypto.go 中的 TrojanPasswordHash)
-	buf.WriteString(TrojanPasswordHash(password))
-	buf.Write([]byte{0x0D, 0x0A}) // CRLF
+	// 1. 密码哈希
+	passHash := TrojanPasswordHash(password)
+	buf.WriteString(passHash)
+	buf.Write([]byte{0x0D, 0x0A}) 
+	log.Printf("[Trojan] 密码哈希已写入")
 
 	// 2. 指令 (0x01 Connect)
 	buf.WriteByte(0x01)
 
-	// 3. 目标地址 (调用 utils.go 中的 ToSocksAddr)
+	// 3. 目标地址
 	addr, err := ToSocksAddr(targetHost, targetPort)
 	if err != nil {
+		log.Printf("[Trojan] 地址解析失败: %v", err)
 		return nil, err
 	}
 	buf.Write(addr)
@@ -26,5 +31,6 @@ func BuildTrojanPayload(password, targetHost string, targetPort int) ([]byte, er
 	// 4. CRLF 结尾
 	buf.Write([]byte{0x0D, 0x0A})
 
+	log.Printf("[Trojan] 握手包构造成功")
 	return buf.Bytes(), nil
 }
